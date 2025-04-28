28c7de178814574a0dc19786a44429a39c0305a1
marcosegan <megan@frba.utn.edu.ar>
2025-04-28 12:05:12 -0300

    Punto 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,50 @@ module Lib () where
 
 import Text.Show.Functions ()
 
+data Personaje = ConstruirPersonaje {
+        nombre :: String,
+        poderBasico :: Poder,
+        superPoder :: Poder,
+        superPoderEstaActivo :: Bool,
+        vida :: Int
+    }
+    deriving Show
+type Poder = Personaje -> Personaje
+
+quitarVida :: Personaje -> Int -> Personaje
+quitarVida personaje danio
+    | vida personaje >= danio = personaje { vida = vida personaje - danio }
+    | otherwise = personaje { vida = 0 }
+
+bolaEspinosa :: Poder
+bolaEspinosa contrincante = quitarVida contrincante 1000
+
+type TipoDeTuerca = String
+
+lluviaDeTuercas :: TipoDeTuerca -> Personaje -> Personaje
+lluviaDeTuercas tipoDeTuerca contrincante
+    | tipoDeTuerca == "Sanadora" = contrincante { vida = vida contrincante + 800 }
+    | tipoDeTuerca == "Dañina" = contrincante { vida = vida contrincante `div` 2 }
+    | otherwise = contrincante
+
+-- Cuando el radio de explosión es mayor a 3, es una potente :O
+granadaDeEspinasPotente :: Poder
+granadaDeEspinasPotente contrincante
+    | vida contrincante < 800 = contrincante {
+            superPoderEstaActivo = False,
+            vida = 0,
+            -- TODO: ¿Por qué estoy repitiendo lo mismo? Debería ser más fácil
+            nombre = nombre contrincante ++ "Espina estuvo aquí"
+        }
+    | otherwise = contrincante { nombre = nombre contrincante ++ "Espina estuvo aquí" }
+
+granadaDeEspinas :: Int -> Personaje -> Personaje
+granadaDeEspinas radioDeExplosion contrincante
+    | radioDeExplosion > 3 = granadaDeEspinasPotente contrincante
+    | otherwise = contrincante
+
+torretaCurativa :: Poder
+torretaCurativa aliado = aliado { superPoderEstaActivo = True, vida = vida aliado * 2 }
+
 doble :: Int -> Int
 doble x = x * 2
